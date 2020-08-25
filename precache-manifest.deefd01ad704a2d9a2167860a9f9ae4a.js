c08c77b82fab827e7a5c7bdb0e164f483d64e314
Stas <nevmyvakastas@gmail.com>
2020-08-25 15:31:33 +0600

    Updates

---
+++ precache-manifest.deefd01ad704a2d9a2167860a9f9ae4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8e3a0a410720959686dde5fe15d4eb",
+    "url": "/terminals-test-app/index.html"
+  },
+  {
+    "revision": "4adf1fa4c4ff44343961",
+    "url": "/terminals-test-app/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "b773eeecc629650bbb9b",
+    "url": "/terminals-test-app/static/js/2.7a389f05.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/terminals-test-app/static/js/2.7a389f05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4adf1fa4c4ff44343961",
+    "url": "/terminals-test-app/static/js/main.2e34599c.chunk.js"
+  },
+  {
+    "revision": "b9effa54854a1f195e03",
+    "url": "/terminals-test-app/static/js/runtime-main.c1c33527.js"
+  },
+  {
+    "revision": "ced2c2f9d05fa93f960ea793823bffda",
+    "url": "/terminals-test-app/static/media/deleteBtn.ced2c2f9.png"
+  },
+  {
+    "revision": "bbd7ea09104a9707bc227ac51f07732e",
+    "url": "/terminals-test-app/static/media/ghicon.bbd7ea09.webp"
+  }
+]);
